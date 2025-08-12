b693fc32842cc6a257ef8cbc63a697502bb18aaa
manjaycoder <manjay.verma.coder@gmail.com>
2025-08-12 15:23:49 +0530

    message

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -14,6 +14,7 @@ const SearchScreen = () => {
   return (
     <SafeAreaView className='flex-1 bg-white'>
       {/* Header */}
+      {/* add a search functionality */}
       <View className='px-4 py-3 border-b border-gray-100'>
         <View className='flex-row items-center bg-gray-100 rounded-full px-4 py-3'>
           <Feather name="search" size={20} color="#657786"/>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
+import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
 import "../global.css"
+const queryClient=new QueryClient()
 export default function RootLayout() {
 
   return (
  <ClerkProvider  tokenCache={tokenCache} >
-
+<QueryClientProvider client={queryClient}>
 <Stack screenOptions={{headerShown:false}}>
   <Stack.Screen name="(auth)"  options={{headerShown:false}}/>
 </Stack>
+</QueryClientProvider>
 
   </ClerkProvider>
 
